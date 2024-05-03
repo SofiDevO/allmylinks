edee2b007e65cc860b6f422d285577721cfc4d5b
SofiDev <angiemeow89@gmail.com>
2024-05-03 01:27:03 -0600

    first commit

---
+++ src/data/userData.js
@@ -0,0 +1,56 @@
+const userData = [
+  {
+    userName: "Sofia Osorio",
+    userDesc: "Web Developer",
+    userPic: "https://github.com/SofiDevO.png",
+  },
+];
+const socialLinks = [
+  {
+    socialName: "LinkedIn",
+    socialUl: "#",
+    socialIcon: "mingcute:linkedin-fill",
+  },
+  {
+    socialName: "GitHub",
+    socialUl: "#",
+    socialIcon: "fa6-brands:github-alt",
+  },
+  {
+    socialName: "Instagram",
+    socialUl: "#",
+    socialIcon: "ri:instagram-fill",
+  },
+  {
+    socialName: "X",
+    socialUl: "#",
+    socialIcon: "prime:twitter",
+  },
+  {
+    socialName: "Whatsapp",
+    socialUl: "#",
+    socialIcon: "mingcute:whatsapp-fill",
+  },
+];
+
+const userLinks = [
+    {
+        title: "Portfolio",
+        link:"# ",
+        icon:"ant-design:code-filled"
+    },
+    {
+        title: "Sucríbete a mi canal",
+        link:"#",
+        icon:"mingcute:youtube-fill"
+
+    },
+    {
+        title: "Únete a Discord",
+        link:"#",
+        icon:"ic:outline-discord"
+    },
+]
+
+
+export { userData, userLinks, socialLinks };
